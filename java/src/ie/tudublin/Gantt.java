82c856838f2c792594c45ef6321e039c1a9a0a7a
unknown <abdul.ismail06@gmail.com>
2020-04-22 11:02:14 +0100

    cleanup code

--- java/src/ie/tudublin/Gantt.java
+++ java/src/ie/tudublin/Gantt.java
@@ -2,42 +2,34 @@ package ie.tudublin;
 
 import processing.core.PApplet;
 
-public class Gantt extends PApplet
-{	
+public class Gantt extends PApplet {	
 	
-	public void settings()
-	{
+	public void settings() {
 		size(800, 600);
 	}
 
-	public void loadTasks()
-	{
+	public void loadTasks() {
 		
 	}
 
-	public void printTasks()
-	{
+	public void printTasks() {
 		
 	}
 	
-	public void mousePressed()
-	{
+	public void mousePressed() {
 		println("Mouse pressed");	
 	}
 
-	public void mouseDragged()
-	{
+	public void mouseDragged() {
 		println("Mouse dragged");
 	}
 
 	
 	
-	public void setup() 
-	{
+	public void setup() {
 	}
 	
-	public void draw()
-	{			
+	public void draw() {			
 		background(0);
 	}
 }
