f1815bbbdae1e7528aa9aee367baabafd0ba07e6
unknown <abdul.ismail06@gmail.com>
2020-04-22 11:04:40 +0100

    private methods & constructor

---
+++ java/src/ie/tudublin/Task.java
@@ -0,0 +1,13 @@
+package ie.tudublin;
+
+public class Task {
+    private String task;
+    private int start;
+    private int end;
+
+    public Task(String task, int start, int end) {
+        this.task = task;
+        this.start = start;
+        this.end = end;
+    }
+}
