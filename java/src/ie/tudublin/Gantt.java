c1c16f5a8631a3a118c1d128b05d40e1a4a90ab8
unknown <abdul.ismail06@gmail.com>
2020-04-22 14:19:23 +0100

    mouse pressed and mouse dragged with minor comments added

--- java/src/ie/tudublin/Gantt.java
+++ java/src/ie/tudublin/Gantt.java
@@ -1,4 +1,4 @@
-package ie.tudublin;
+lpackage ie.tudublin;
 
 import java.util.ArrayList;
 
@@ -8,14 +8,31 @@ import processing.data.TableRow;
 
 public class Gantt extends PApplet {	
 
+	// array lists
 	private ArrayList<Task> tasks = new ArrayList<Task>();
+
+	// floats
 	private float leftMargin;
 	private float margin;
-	
+	private float rectHeight;
+
+	// integers
+	private int leftRectSelected;
+	private int rightRectSelected;
+	private int numOfDays;
+
 	public void settings() {
 		size(800, 600);
+
+		// floats
 		leftMargin = width / 6;
 		margin = width / 20;
+		rectHeight = 35;
+
+		// integers
+		leftRectSelected = -1;
+		rightRectSelected = -1;
+		numOfDays = 30;
 	}
 
 	public void loadTasks() {
@@ -34,9 +51,7 @@ public class Gantt extends PApplet {
 	}
 
 	public void displayTasks() {
-		int numOfDays = 30;
 		float textHeight = 0.8f;
-		float rectHeight = 35;
 		float radius = 5;
 		
 		stroke(255);
@@ -53,7 +68,7 @@ public class Gantt extends PApplet {
 		noStroke();
 
 		for(int i = 0; i < tasks.size(); i++) {
-			// we need to set fill back to white so all texts can appear as white
+			// set fill back to white so all texts can appear as white
 			fill(255);
 
 			float y = map(i, 0, tasks.size(), 2 * margin, height - margin);
@@ -66,16 +81,53 @@ public class Gantt extends PApplet {
 			float rectEnd = map(tasks.get(i).getEnd(), 1, numOfDays, leftMargin, width - margin);
 			float rectWidth = rectEnd - rectStart;
 
-			rect(rectStart, y - rectHeight / 2, rectWidth, rectHeight, radius); 
+			rect(rectStart, y - (rectHeight / 2), rectWidth, rectHeight, radius); 
 		}
 	}
 	
 	public void mousePressed() {
-		println("Mouse pressed");	
+		float pixels = 20;
+
+		for(int i = 0; i < tasks.size(); i++) {
+			Task task = tasks.get(i);
+
+			float rectStart = map(task.getStart(), 1, numOfDays, leftMargin, width - margin);
+			float rectEnd = map(task.getEnd(), 1, numOfDays, leftMargin, width - margin);
+			float rectTop = map(i, 0, tasks.size(), 2 * margin, height - margin) - rectHeight / 2;
+			float rectBottom = rectTop + rectHeight;
+
+			if(mouseY >= rectTop && mouseY <= rectBottom) {
+				if(mouseX > rectStart - pixels && mouseX < rectStart + pixels) {
+					leftRectSelected = i;
+					rightRectSelected = -1;
+				}
+
+				if(mouseX > rectEnd - pixels && mouseX < rectEnd + pixels) {
+					leftRectSelected = -1;
+					rightRectSelected = i;
+				}
+			}
+		}
 	}
 
 	public void mouseDragged() {
-		println("Mouse dragged");
+		if(leftRectSelected > -1) {
+			Task task = tasks.get(leftRectSelected);
+			int date = (int) map(mouseX, 0, width, 0, numOfDays);
+
+			if(date > 0 && date < task.getEnd() && task.getEnd() - date >= 1)  {
+				task.setStart(date);
+			}
+		}
+
+		if(rightRectSelected > -1) {
+			Task task = tasks.get(rightRectSelected);
+			int date = (int) map(mouseX, 0, width, 0, numOfDays);
+
+			if(date <= numOfDays && date > task.getStart() && date - task.getStart() >= 1) {
+				task.setEnd(date);
+			}
+		}
 	}
 	
 	public void setup() {
