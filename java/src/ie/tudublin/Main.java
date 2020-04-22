24e7e1480c718a54c666eeaba09b79ad16c604b9
unknown <abdul.ismail06@gmail.com>
2020-04-22 11:00:40 +0100

    test - first commit

--- java/src/ie/tudublin/Main.java
+++ java/src/ie/tudublin/Main.java
@@ -1,16 +1,12 @@
 package ie.tudublin;
 
-public class Main
-{
-
-    public void gant()
-	{
+public class Main {
+    public void gant() {
 		String[] a = {"MAIN"};
         processing.core.PApplet.runSketch( a, new Gantt());
     }
     
-    public static void main(String[] arg)
-    {
+    public static void main(String[] arg) {
         Main main = new Main();
 		main.gant();        
     }
