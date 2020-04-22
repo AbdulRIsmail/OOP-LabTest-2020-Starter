a0a0555138a6d55b94ef8672e52f9c94bba1ebd2
unknown <abdul.ismail06@gmail.com>
2020-04-22 12:06:01 +0100

    display text date and lines vertically

--- java/src/ie/tudublin/Gantt.java
+++ java/src/ie/tudublin/Gantt.java
@@ -9,9 +9,13 @@ import processing.data.TableRow;
 public class Gantt extends PApplet {	
 
 	private ArrayList<Task> tasks = new ArrayList<Task>();
+	private float leftMargin;
+	private float margin;
 	
 	public void settings() {
 		size(800, 600);
+		leftMargin = width / 6;
+		margin = width / 20;
 	}
 
 	public void loadTasks() {
@@ -28,6 +32,21 @@ public class Gantt extends PApplet {
 			System.out.println(t);
 		}
 	}
+
+	public void displayTasks() {
+		int numOfDays = 30;
+		float textHeight = 0.8f;
+		
+		stroke(255);
+		fill(255);
+		textAlign(CENTER);
+
+		for(int i = 1; i <= numOfDays; i++) {
+			float x = map(i, 1, 30, leftMargin, width - margin);
+			line(x, margin, x, height - margin);
+			text(i, x, margin * textHeight);
+		}
+	}
 	
 	public void mousePressed() {
 		println("Mouse pressed");	
@@ -36,15 +55,14 @@ public class Gantt extends PApplet {
 	public void mouseDragged() {
 		println("Mouse dragged");
 	}
-
-	
 	
 	public void setup() {
 		loadTasks();
 		printTasks();
 	}
 	
-	public void draw() {			
+	public void draw() {
 		background(0);
+		displayTasks();
 	}
 }
